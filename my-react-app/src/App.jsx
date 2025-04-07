71259fbd71bcadbe846b081219f9dabab6a537a2
olivier Rukundo <rr.olivier01@gmail.com>
2025-04-07 13:23:45 +0200

    second time

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// inside the terminal: npm install redux react-redux
+// useSelector: A React-Redux hook used to read values from the Redux store.
+// useDispatch: A React-Redux hook used to send actions to the Redux store (to update state).
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
 
 function App() {
- 
+    const count = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+
+    return(
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={() => dispatch({ type: 'INCREMENT'})} >+</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT'})}>-</button>
+        </div>
+    );
 }
 
 export default App
